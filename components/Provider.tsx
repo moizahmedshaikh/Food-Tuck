025e27e619eb6df3232f8a5c25d538cf8aace8b1
Moiz Ahmed <moizaman905@gmail.com>
2025-02-12 23:11:11 +0500

    updated

--- components/Provider.tsx
+++ components/Provider.tsx
@@ -3,14 +3,15 @@
 import { ReactNode } from "react";
 import { CartProvider as USCProvider } from "use-shopping-cart";
 
+// card prvider
 const CartProvider = ({ children }: { children: ReactNode }) => {
   return (
     <USCProvider
       mode="payment"
       cartMode="client-only"
       stripe={process.env.NEXT_PUBLIC_STRIPE_KEY as string}
-      successUrl="https://hakathon3-market-place-food-web.vercel.app/success"
-      cancelUrl="https://hakathon3-market-place-food-web.vercel.app/error"
+      successUrl="https://food-tuck-web.vercel.app/success"
+      cancelUrl="https://food-tuck-web.vercel.app/error"
       currency="USD"
       billingAddressCollection={false}
       shouldPersist={true}
